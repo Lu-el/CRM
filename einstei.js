d6bb5ca70a083d157c805145f48abb61e2184bd1
Lu-el <lubov.elunina@mail.ru>
2022-05-31 20:43:50 +0400

    JS4-2

---
+++ einstei.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myWeight = 4.5;
+const speedLight = 3e8;
+
+const energyBody = myWeight * speedLight ** 2;
+console.log(`Энергия моего кота составляет ${energyBody} у.е.`);
--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 'use strict';
 
-const productName = 'кофеварка';
-const productAmount = 15;
-const category = 'техника для кухни';
-const price = 5699;
+{
+  const productName = 'кофеварка';
+  const productAmount = 15;
+  const category = 'техника для кухни';
+  const price = 5699;
 
-const orderSum = productAmount * price;
+  const orderSum = productAmount * price;
 
-console.log(productName);
-console.log(orderSum);
+  console.log(productName);
+  console.log(orderSum);
+}
+
+{
+  const productName = prompt('Введите название товара');
+  const productAmount = prompt('Введите кол-во товара (только целое число)');
+  const category = prompt('Введите название категории');
+  const price = prompt('Введите стоимость товара');
+  const orderSum = productAmount * price;
+
+  console.log(typeof productName);
+  console.log(typeof +productAmount);
+  console.log(typeof category);
+  console.log(typeof +price);
+
+  console.log(
+    `Вы решили приобрести ${productAmount} единицы товара "${productName}",
+    в общей сложности на сумму ${orderSum} вьетнамских донг.
+    Сумма у вас будет списана автоматически)`);
+}
