126f58da2941378f40e90af3005e8fb55e39e66f
Lu-el <lubov.elunina@mail.ru>
2022-05-31 18:06:01 +0400

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const productName = 'кофеварка';
+const productAmount = 15;
+const category = 'техника для кухни';
+const price = 5699;
+
+const orderSum = productAmount * price;
+
+console.log(productName);
+console.log(orderSum);
