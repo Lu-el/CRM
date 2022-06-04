63e217a9ffbd0d1ce5d09697ffe7534e72c3f14a
Lu-el <lubov.elunina@mail.ru>
2022-06-04 17:04:41 +0400

    correcting Hard

--- tasksHard_2.js
+++ tasksHard_2.js
@@ -1,26 +1,31 @@
 'use strict';
-
-{
-  const income = +prompt('Сумма вашего дохода(усложненный вариант):56');
+15
+  const income = +prompt('Сумма вашего дохода(усложненный вариант):');
 
   let taxes = 0;
   const tax1 = 0.13;
   const tax2 = 0.2;
   const tax3 = 0.3;
 
-  if (!Number.isNaN(income)) {
-    if (income < 15000 && income > 0) {
-      taxes = income * tax1;
-    } else if (income >= 15000 && income <= 50000) {
-      taxes = (income - 15000) * tax2 + 15000 * tax1;
-    } else if (income > 50000) {
-      taxes = (income - 50000) * tax3 + 35000 * tax2 + 15000 * tax1;
-    } else {
-      taxes = 0;
-    }
-
-    console.log(`Сумма налога составляет(усложненный вариант): ${taxes}`);
-  } else {
-    console.log(`Введены некорректные данные`);
+if (!Number.isNaN(income)) {
+  let rest = 0;
+  if (income > 50000) {
+    rest = income - 50000;
+    taxes = rest * tax3;
+    rest = 35000;
   }
+  if (income >= 15000) {
+    rest = rest || (income - 15000);
+    taxes += rest * tax2;
+    rest = 15000;
+  }
+  if (income > 0) {
+    rest = rest || income;
+    taxes += rest * tax1;
+  }
+
+  console.log(`Сумма налога составляет(усложненный вариант): ${taxes}`);
+} else {
+  console.log(`Введены некорректные данные`);
 }
+
