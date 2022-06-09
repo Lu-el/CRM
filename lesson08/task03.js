8608276395d14fd62c26bac3a04812cf6787bb93
Lu-el <lubov.elunina@mail.ru>
2022-06-09 09:59:20 +0400

    correction1

--- lesson08/task03.js
+++ lesson08/task03.js
@@ -5,11 +5,23 @@ const generateRandomNumbers = (count, n, m, str) => {
   const maximum = n > m ? n : m;
   const minimum = n < m ? n : m;
 
-  const randomNumbers = array.map(elem =>
-    elem = Math.round(Math.random() * (maximum - minimum)) + minimum);
-
-  const condition = str === 'even' ? 0 : 1;
-  const filterNumbers = randomNumbers.filter(elem => Math.abs(elem % 2) === condition);;
-  return filterNumbers;
+  if (str === 'even' || str === 'odd') {
+    const condition = str === 'even' ? 0 : 1;
+    const newArray = [...Array(Math.abs((maximum - minimum)) + 1)];
+    const numbersArray = newArray.map((elem, i) => elem = minimum + i);
+    const filterNumbers = numbersArray.filter(elem => Math.abs(elem % 2) === condition);
+    const counter = filterNumbers.length;
+    const randomNumbers = [];
+    for (let i = 0; i < count; i++) {
+      const randomIndex = Math.trunc(Math.random() * counter);
+      randomNumbers.push(filterNumbers[randomIndex]);
+    }
+    return randomNumbers;
+  } else {
+    const randomNumbers = array.map(elem =>
+      elem = Math.round(Math.random() * (maximum - minimum)) + minimum);
+    return randomNumbers;
+  }
 }
+
 
--- lesson08/task04.js
+++ lesson08/task04.js
@@ -6,7 +6,20 @@ const getLeapYear = (n, m) => {
 
   const array = [...Array(maximum - minimum + 1)];
   const allYears = array.map((elem, i) => elem = minimum + i);
-  const leapYears = allYears.filter(elem => !(elem % 4));
+
+  const leapYears = allYears.filter(elem => {
+    if (elem % 100) {
+      if (!(elem % 4)) {
+        return elem;
+      }
+    } else {
+      if (!(elem % 400)) {
+        return elem;
+      }
+    }
+  });
   return leapYears;
 }
+
+console.log(getLeapYear(-5, 12));
 
